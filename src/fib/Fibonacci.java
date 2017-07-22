5bf297e95591ec15bab4869eeb9e06c533bfda0d
Ezeh Emmanuel <emmanuel.ezeh@insightafrica.com.ng>
2017-07-22 13:08:38 +0100

    First commit on Fibonacci problem

---
+++ src/fib/Fibonacci.java
@@ -0,0 +1,53 @@
+package fib;
+
+import java.util.Calendar;
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * Created by emmanuel on 7/21/17.
+ */
+public class Fibonacci {
+
+
+    public static long fib(long n){
+        if(n < 2){
+            return 1;
+        }
+
+        return fib(n-1) + fib(n-2);
+    }
+
+    static Map<Long, Long> memo = new HashMap<>();
+
+
+    //An optimised fibonacci function that runs faster and more efficiently
+    public static long optimisedFib(long n){
+
+        if(memo.containsKey(n)){
+            return memo.get(n);
+        }
+
+        if(n < 2){
+            memo.put(n, 1l);
+            return 1;
+        }
+
+        long f = optimisedFib(n-1) + optimisedFib(n-2);
+        memo.put(n, f);
+
+        return f ;
+
+    }
+
+    public static  void main(String[] args){
+        long startTime = Calendar.getInstance().getTimeInMillis();
+        System.out.printf("%,12d%n", fib(10));
+        long endTime = Calendar.getInstance().getTimeInMillis();
+
+        System.out.println("Duration: " + (endTime - startTime));
+    }
+
+
+
+}
