13534a952c0fb2a6a50bba3df0ebb79d94992e4b
Ezeh Emmanuel <ezeh1693@gmail.com>
2024-02-03 10:34:48 +0100

    Minor changes to code structure

--- src/fib/Fibonacci.java
+++ src/fib/Fibonacci.java
@@ -8,8 +8,6 @@ import java.util.Map;
  * Created by emmanuel on 7/21/17.
  */
 public class Fibonacci {
-
-
     public static long fib(long n){
         if(n < 2){
             return n;//return n when n < 2;
@@ -18,36 +16,36 @@ public class Fibonacci {
         return fib(n-1) + fib(n-2);
     }
 
-    static Map<Long, Long> memo = new HashMap<>();
-
-
     //An optimised fibonacci function that runs faster and more efficiently
-    public static long optimisedFib(long n){
+    public static long optimisedFib(int n, Map<Integer, Long> memo){
 
         if(memo.containsKey(n)){
             return memo.get(n);
         }
 
         if(n < 2){
-            memo.put(n, n);
             return n;
         }
 
-        long f = optimisedFib(n-1) + optimisedFib(n-2);
+        long f = optimisedFib(n-1, memo) + optimisedFib(n-2, memo);
         memo.put(n, f);
 
         return f ;
-
     }
 
     public static  void main(String[] args){
         long startTime = Calendar.getInstance().getTimeInMillis();
-        System.out.printf("%,12d%n", optimisedFib(10));
+        System.out.println("With basic Fib:");
+        System.out.println("Result: " + fib(10));
         long endTime = Calendar.getInstance().getTimeInMillis();
+        System.out.println("Duration: " + (endTime - startTime));
+        System.out.println();
 
+        //With Optimised fib
+        startTime = Calendar.getInstance().getTimeInMillis();
+        System.out.println("With Optimised Fib:");
+        System.out.println("Result: " + optimisedFib(10, new HashMap<>()));
+        endTime = Calendar.getInstance().getTimeInMillis();
         System.out.println("Duration: " + (endTime - startTime));
     }
-
-
-
 }
